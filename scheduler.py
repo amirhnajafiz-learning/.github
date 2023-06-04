c36fd8f89a3076784e9f6f547395da9eadd0461c
amirhnajafiz <najafizadeh21@gmail.com>
2023-06-04 17:21:40 +0330

    fix: job doing

--- scheduler.py
+++ scheduler.py
@@ -11,7 +11,7 @@ class Scheduler(object):
         """
         self.taskSet = TaskSet(data)
         
-        self.resources = []
+        self.resources = {}
     
     def size(self):
         """return the size of task set
@@ -38,9 +38,12 @@ class Scheduler(object):
             jobs = [job for job in self.taskSet.getJobs() if job.isActive(time)] # get all active jobs
             jobs.sort(key=lambda x: x.getFP(), reverse=True) # sort jobs by deadline monothonic
             
-            if len(jobs) > 0: # schedule
+            if len(jobs) > 0: # schedule when we have jobs
                 currentJob = jobs[0]
-            
+                
+            if currentJob != None: # do the job
+                currentJob.doJob()
+
             jobsList[time] = currentJob
         
         return jobsList
