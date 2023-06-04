31ffc254ea8896858ac836f8714aa3c0049adb70
amirhnajafiz <najafizadeh21@gmail.com>
2023-06-04 12:55:29 +0330

    add: main

---
+++ main.py
@@ -0,0 +1,26 @@
+import sys
+import json
+
+from scheduler import Scheduler
+
+
+
+if __name__ == "__main__":
+    # get file path and time
+    if len(sys.argv) > 1:
+        file_path = sys.argv[1]
+        time = int(sys.argv[2])
+    else:
+        file_path = "taskset.json"
+        time = 50
+
+    with open(file_path) as json_data:
+        data = json.load(json_data)
+    
+    # create scheduler
+    s = Scheduler(data=data)
+    jobs = s.run(time)
+    
+    for t, j in jobs.items():
+        if j != None:
+            print(f'{t} | {j}')
--- scheduler.py
+++ scheduler.py
@@ -24,15 +24,16 @@ class Scheduler(object):
         """
         jobsList = {}
         
-        for time in limit:
-            job = None
+        for time in range(limit):
+            currentJob = None
+            jobs = [job for job in self.taskSet.getJobs()] # get all jobs
             
-            jobs = [job for job in self.taskSet.getJobs() if job.isActive(time)] # get all active jobs
-            jobs.sort(key=lambda x: x.getFP(), reverse=True) # sort jobs by deadline monothonic
+            activeJobs = [job for job in jobs if job.isActive(time)] # get all active jobs
+            activeJobs.sort(key=lambda x: x.getFP(), reverse=True) # sort jobs by deadline monothonic
             
-            if len(jobs) > 0: # schedule
-                job = jobs[0]
+            if len(activeJobs) > 0: # schedule
+                currentJob = activeJobs[0]
             
-            jobsList[time] = job
+            jobsList[time] = currentJob
         
         return jobsList
